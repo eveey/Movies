38c829643f4f22997284fc16f54d809a3bedba8f
eveey <eva.shtos@gmail.com>
2018-09-21 23:44:34 +0200

    Create activities, add icons

---
+++ app/src/main/kotlin/com/evastos/movies/ui/movie/base/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.evastos.movies.ui.movie.base
+
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()
---
+++ app/src/main/kotlin/com/evastos/movies/ui/movie/overview/MovieOverviewActivity.kt
@@ -0,0 +1,13 @@
+package com.evastos.movies.ui.movie.overview
+
+import android.os.Bundle
+import com.evastos.movies.R
+import com.evastos.movies.ui.movie.base.BaseActivity
+
+class MovieOverviewActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_overview)
+    }
+}
