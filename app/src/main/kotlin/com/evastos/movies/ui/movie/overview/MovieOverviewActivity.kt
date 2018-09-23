a2636e4d964a22283f57a7809fd90f76425feaad
eveey <eva.shtos@gmail.com>
2018-09-23 14:43:31 +0200

    Create view models

--- app/src/main/kotlin/com/evastos/movies/ui/movie/overview/MovieOverviewActivity.kt
+++ app/src/main/kotlin/com/evastos/movies/ui/movie/overview/MovieOverviewActivity.kt
@@ -1,13 +1,26 @@
 package com.evastos.movies.ui.movie.overview
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import com.evastos.movies.R
 import com.evastos.movies.ui.movie.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_movie_overview.moviesRecyclerView
 
 class MovieOverviewActivity : BaseActivity() {
+
+    companion object {
+        private const val COLUMNS_COUNT = 2
+    }
+
+    private lateinit var movieOverviewViewModel: MovieOverviewViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_overview)
+        moviesRecyclerView.layoutManager = GridLayoutManager(this, COLUMNS_COUNT)
+
+        movieOverviewViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(MovieOverviewViewModel::class.java)
     }
 }
--- app/src/main/kotlin/com/evastos/movies/ui/movie/overview/MovieOverviewViewModel.kt
+++ app/src/main/kotlin/com/evastos/movies/ui/movie/overview/MovieOverviewViewModel.kt
@@ -1,5 +1,7 @@
 package com.evastos.movies.ui.movie.overview
 
+import android.arch.lifecycle.MutableLiveData
+import com.evastos.movies.data.model.moviedb.Movie
 import com.evastos.movies.data.rx.applySchedulers
 import com.evastos.movies.domain.Repositories
 import com.evastos.movies.ui.movie.base.BaseViewModel
@@ -11,11 +13,23 @@ class MovieOverviewViewModel
     private val movieOverviewRepository: Repositories.MovieOverviewRepository
 ) : BaseViewModel() {
 
+    val moviesLiveData = MutableLiveData<List<Movie>>()
+
     init {
         disposables.add(movieOverviewRepository.getNowPlayingMovies()
                 .applySchedulers()
                 .subscribe { t1, t2 ->
                     Timber.i(t1.toString())
+
+                    t1.results?.let {
+                        if (moviesLiveData.value == null) {
+                            moviesLiveData.postValue(it.toMutableList())
+                        } else {
+                            val moviesList = moviesLiveData.value!!.toMutableList()
+                            moviesList.addAll(it)
+                            moviesLiveData.postValue(moviesList.toMutableList())
+                        }
+                    }
                     Timber.e(t2)
                 })
 
